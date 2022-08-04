dab1c05862f8cf669feba4a4776c6f06fb1facf6
ducanh2004 <110523685+ducanh2004@users.noreply.github.com>
2022-08-04 21:49:52 +0700

    nộp bài tập

---
+++ ss7.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a,b,max;
+    printf("nhap so thu nhat:");
+    scanf("%d",&a);
+    printf("nhap so thu hai:");
+    scanf("%d,&b");
+    max=a;
+    if(max<b)
+    max=b;
+    printf("so lon nhat la %d",max);
+}
